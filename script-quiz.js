097456b0ee0e5d085a6942f8b7ccc5cd1f86b3f0
Jayu <jayeshhande00@gmail.com>
2021-12-17 19:00:22 +0530

    quiz.html

---
+++ script-quiz.js
@@ -0,0 +1,7 @@
+const quizform = document.querySelector(".container");
+
+const button = document.querySelector("#submit-btn");
+
+const output = document.querySelector("#output");
+
+
--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const button = document.querySelector("button");
 const dom = document.querySelector("#output");
 
 function isTriangle(){
-    console.log("clicked")
-    const sumOfAngles = calculateSum(Number(angles[0]),Number(angles[1]),Number(angles[2]));
+    const sumOfAngles = calculateSum(Number(angles[0].value),Number(angles[1].value),Number(angles[2].value));
+
     if(sumOfAngles === 180){
-        dom.innerText = "Yes its form a triangle"
+        dom.innerText = "Yes!! Angles form a triangle"
     }
     else{
-        dom.innerText = "no its  not form a triangle"
+        dom.innerText = "NO!! Angles don't form a triangle"
     }
 }
 
 function calculateSum(angle1,angle2,angle3){
-    const sum =  angle1+ angle2+angle3;
-    return sum;
+
+    const sumOfAngles =  angle1 + angle2 + angle3;
+    return sumOfAngles;
 }
 
 button.addEventListener("click",isTriangle)
