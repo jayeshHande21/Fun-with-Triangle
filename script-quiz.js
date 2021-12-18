fd4103fca55bf33d1230ae5b760449b468bc782e
Jayu <jayeshhande00@gmail.com>
2021-12-18 11:13:10 +0530

    script for quiz

--- script-quiz.js
+++ script-quiz.js
@@ -2,15 +2,26 @@ const quizform = document.querySelector(".container");
 
 const button = document.querySelector("#submit-btn");
 
-const output = document.querySelector("#output");
+const output = document.querySelector(".output");
 
+const correctAnswer = ["90°" , "right angled" , "equilateral triangle"];
 
-function correctAnswer(){
-    console.log("mine is mine")
+function CalculateAnswer(){
+    let score = 0;
+    let index = 0;
+    let formResults = new FormData(quizform); 
+    for (let  value of formResults.values()){
+      if(value === correctAnswer[index]){
+          score = score + 1;
+      }
+      index = index + 1;
+    }
+    output.innerText = "Your Final Score is " + score ;
+    
 }
 
 
 
-button.addEventListener("click" , correctAnswer)
+button.addEventListener("click" , CalculateAnswer)
 
 
