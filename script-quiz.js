9ae16f7adc9691edf3d5c6af89bbacf6cfd34f15
Jayu <jayeshhande00@gmail.com>
2022-05-22 12:53:01 +0530

    add quiz-questions

--- script-quiz.js
+++ script-quiz.js
@@ -4,24 +4,30 @@ const button = document.querySelector("#submit-btn");
 
 const output = document.querySelector(".output");
 
-const correctAnswer = ["90°" , "right angled" , "equilateral triangle"];
+const correctAnswer = [
+  "90°",
+  "right angled",
+  "equilateral triangle",
+  "midsegment",
+  "altitude",
+  "centroid",
+  "concurrent",
+  "one-half",
+  "orthocenter",
+  "2",
+];
 
-function CalculateAnswer(){
-    let score = 0;
-    let index = 0;
-    let formResults = new FormData(quizform); 
-    for (let  value of formResults.values()){
-      if(value === correctAnswer[index]){
-          score = score + 1;
-      }
-      index = index + 1;
+function CalculateAnswer() {
+  let score = 0;
+  let index = 0;
+  let formResults = new FormData(quizform);
+  for (let value of formResults.values()) {
+    if (value === correctAnswer[index]) {
+      score = score + 1;
     }
-    output.innerText = "Your Final Score Is   " + score ;
-    
+    index = index + 1;
+  }
+  output.innerText = "Your Final Score Is   " + score;
 }
 
-
-
-button.addEventListener("click" , CalculateAnswer)
-
-
+button.addEventListener("click", CalculateAnswer);
