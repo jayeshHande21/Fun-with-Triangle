593322bb48f42498cc43c6832934d6c1e2a85574
Jayu <jayeshhande00@gmail.com>
2022-09-19 17:55:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const angles = document.querySelectorAll(".angleinput")
+const angles = document.querySelectorAll(".angleinput");
 const button = document.querySelector("button");
 
 const dom = document.querySelector("#output");
 
-function isTriangle(){
-    const sumOfAngles = calculateSum(Number(angles[0].value),Number(angles[1].value),Number(angles[2].value));
+function isTriangle() {
+  const sumOfAngles = calculateSum(
+    Number(angles[0].value),
+    Number(angles[1].value),
+    Number(angles[2].value)
+  );
 
-    if(sumOfAngles === 180){
-        dom.innerText = "Yes!! Angles form a triangle"
-    }
-    else{
-        dom.innerText = "NO!! Angles don't form a triangle"
-    }
+  if (sumOfAngles === 180) {
+    dom.innerText = "Yes!! Angles form a triangle";
+  } else {
+    dom.innerText = "NO!! Angles don't form a triangle";
+  }
 }
 
-function calculateSum(angle1,angle2,angle3){
-
-    const sumOfAngles =  angle1 + angle2 + angle3;
-    return sumOfAngles;
+function calculateSum(angle1, angle2, angle3) {
+  const sumOfAngles = angle1 + angle2 + angle3;
+  return sumOfAngles;
 }
 
-button.addEventListener("click",isTriangle)
+button.addEventListener("click", isTriangle);
